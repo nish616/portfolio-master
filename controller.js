c40c2df9a1ed14060180fff51181bd69d0db6b8a
Nishin S <nishin616@gmail.com>
2024-01-12 23:40:02 +0530

    added torch effect for cursor

--- controller.js
+++ controller.js
@@ -1,3 +1,9 @@
 window.onload = () => {
+	const about = document.getElementById("about");
+	const torch = document.getElementById("torch");
 
+	about.addEventListener('mousemove', function (e) {
+		torch.style.top = e.pageY - 190 + 'px';
+		torch.style.left = e.pageX - 120 + 'px';
+	});
 }
