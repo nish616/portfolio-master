502ddd30beb9cda5435b5506d2d473a4c017d8c5
Nishin S <nishin616@gmail.com>
2024-03-31 21:45:15 +0530

    version 1 commit

--- controller.js
+++ controller.js
@@ -1,9 +1,52 @@
 window.onload = () => {
-	const about = document.getElementById("about");
-	const torch = document.getElementById("torch");
 
-	about.addEventListener('mousemove', function (e) {
-		torch.style.top = e.pageY - 190 + 'px';
-		torch.style.left = e.pageX - 120 + 'px';
+	window.scrollTo(0, 0);
+
+	const sections = [{
+		section: document.querySelector('.section-2'),
+		title: document.querySelector('.section-2 .title h2')
+	}, {
+		section: document.querySelector('.section-3'),
+		title: document.querySelector('.section-3 .title h2')
+	}];
+
+	function isElementInViewport(el) {
+		const rect = el.getBoundingClientRect();
+		return (
+			rect.top >= 0 &&
+			rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+		);
+	}
+
+	function debounce(func, timeout = 300) {
+		let timer;
+		return (...args) => {
+			clearTimeout(timer);
+			timer = setTimeout(() => { func.apply(this, args); }, timeout);
+		};
+	}
+
+	function handleScroll() {
+		for (const section of sections) {
+			// console.log(section);
+			if (isElementInViewport(section.section)) {
+				if (!section.title.classList.contains('section-visible')) {
+					console.log("Added");
+					section.title.classList.add('section-visible');
+				}
+			} else {
+				if (section.title.classList.contains('section-visible')) {
+					section.title.classList.remove('section-visible');
+				}
+			}
+		}
+	}
+
+	// Initial check in case the section is already in the viewport on page load
+	handleScroll();
+
+	// Add scroll event listener
+	window.addEventListener('scroll', function (event) {
+		debounce(handleScroll());
 	});
 }
